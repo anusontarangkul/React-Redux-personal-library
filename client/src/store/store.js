31177c4cf7fce8a46b7fe38ac66270cfde4d28b3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-10-12 13:46:21 -0700

    add bookreducer

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import bookReducer from './reducers/bookReducer'
 
 const store = configureStore({
-    reducer: {}
+    reducer: { books: bookReducer }
 })
 
 export default store;
